a3ba6bc34e9f4601ebcc3c6de8662d8d626c69c0
Jonathanreyes4 <126529680+Jonathanreyes4@users.noreply.github.com>
2023-02-27 22:58:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Gato", 3);
-        cat.makeSound(); // salida: "El gato maúlla"
-
-        int base = 2;
-        int exponent = 4;
-        int result = cat.power(base, exponent);
-        System.out.println(base + " elevado a la " + exponent + " es igual a " + result);
+        Gato gato = new Gato("Misi", 3);
+        gato.emitirSonido(); 
+        int resultado = gato.calcularPotencia(2, 3);
+        System.out.println(resultado); 
     }
 }
-
 
